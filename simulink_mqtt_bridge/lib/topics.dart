a98e1f3f011146f9ec8882eae561533f4de49c1e
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-12 21:42:23 +0530

    adds topics.dart

---
+++ simulink_mqtt_bridge/lib/topics.dart
@@ -0,0 +1,20 @@
+enum Topics {
+  outputVoltage,
+  outputCurrent,
+  inputVoltage,
+  inputCurrent,
+  inputPower,
+  outputPower,
+  efficiency,
+  maxVoltage,
+  maxCurrent,
+  maxPower,
+  load,
+}
+
+extension TopicsStringifier on Topics {
+  String get topic {
+    final value = toString().split('.').last;
+    return 'ps/state/${value[0].toUpperCase()}${value.substring(1)}';
+  }
+}
