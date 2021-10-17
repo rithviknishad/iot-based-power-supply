d5d86139c0dc716aa58852683bbe8a36eacaef08
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-17 09:54:49 +0530

    inject timestamp too to packet

--- simulink_mqtt_bridge/bin/simulink_mqtt_bridge.dart
+++ simulink_mqtt_bridge/bin/simulink_mqtt_bridge.dart
@@ -1,22 +1,60 @@
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:simulink_mqtt_bridge/simulink.dart' as simulink;
 import 'package:simulink_mqtt_bridge/mqtt.dart' as mqtt;
+import 'package:simulink_mqtt_bridge/topics.dart';
 
-void main() async {
-  final server = await ServerSocket.bind(InternetAddress.anyIPv4, 8081);
+void main(List<String> args) async {
+  final parser = ArgParser()
+    ..addOption(
+      'listen',
+      defaultsTo: '8081',
+      help: 'The port which the ServerSocket should listen to.',
+    )
+    ..addOption(
+      'mqtt-server',
+      defaultsTo: 'ws://xtensablade.ddns.net',
+      help: 'The address of the MQTT server',
+    )
+    ..addOption(
+      'mqtt-username',
+      defaultsTo: '',
+      help:
+          'The username that is to be used to establish connection to the MQTT server.',
+    )
+    ..addOption(
+      'mqtt-password',
+      defaultsTo: '',
+      help:
+          'The password that is to be used to establish connection to the MQTT server.',
+    )
+    ..addOption(
+      'mqtt-port',
+      defaultsTo: '8080',
+      help: "MQTT server's port",
+    );
 
-  stdout.writeln(
-    'socket server listening at: ${server.address.address}:${server.port}',
+  final argResults = parser.parse(args);
+
+  final server = await ServerSocket.bind(
+    InternetAddress.anyIPv4,
+    int.tryParse(argResults['listen']) ?? 8081,
   );
 
   await mqtt.connect(
-    'ws://xtensablade.ddns.net',
-    port: 8080,
+    argResults['mqtt-server'] ?? 'ws://xtensablade.ddns.net',
+    port: int.tryParse('mqtt-port') ?? 8080,
     as: 'simulink-push-pull-${server.hashCode}',
+    username: argResults['mqtt-username'],
+    password: argResults['mqtt-password'],
   );
 
   server.listen(handleConnection);
+
+  stdout.writeln(
+    'SocketServer listening at: ${server.address.address}:${server.port}',
+  );
 }
 
 void handleConnection(Socket client) {
@@ -24,9 +62,14 @@ void handleConnection(Socket client) {
 
   client.listen(
     (data) => mqtt.updateState(
-      simulink.parsePacket(data).map(
-            (topic, value) => MapEntry(topic, value.toStringAsFixed(4)),
-          ),
+      {
+        // All attributes
+        ...simulink.parsePacket(data).map(
+              (topic, value) => MapEntry(topic, value.toStringAsFixed(4)),
+            ),
+        // Timestamp
+        Topics.lastUpdatedOn: DateTime.now().toString(),
+      },
     ),
 
     // Log the error.
