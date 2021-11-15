222c4cf934b4bfad1f84cd0b7e35eb8b2d20019b
Rithvik Nishad <rithvikn2001@gmail.com>
2021-11-15 14:30:49 +0530

    fix bool type issue in bridge server

--- simulink_mqtt_bridge/bin/simulink_mqtt_bridge.dart
+++ simulink_mqtt_bridge/bin/simulink_mqtt_bridge.dart
@@ -95,7 +95,7 @@ void handleConnection(Socket client) {
       client.close();
 
       mqtt.updateState({
-        Topics.isRunning: '0',
+        Topics.isRunning: 'false',
       });
     },
   );
