de81d34535c305f533b1868aa31a078af61389fe
Rithvik Nishad <rithvikn2001@gmail.com>
2021-12-01 15:47:49 +0530

    updates

--- sim/iot_ppc_broadcast_to_mqtt.m
+++ sim/iot_ppc_broadcast_to_mqtt.m
@@ -1,11 +1,7 @@
-% This potentially extrinsic function maybe invoked hundreds of times a second.
-% When modifying the method, keep in mind about performance and time complexity.
-%
-% Time Complexity = O(1)
+% Connects to MQTT server on first invocation and publishes the data to 
+% MQTT Ecosystem to it's respective topics.
 function iot_ppc_broadcast_to_mqtt(data)
     
-    % Not crazy to reinitialize client and topics 1000 times a second.
-    % So marked as persistent.
     persistent client;
     persistent topics;
     
@@ -13,14 +9,8 @@ function iot_ppc_broadcast_to_mqtt(data)
     if isempty(client)
 
         % Connect to Mosquitto server hosted at `rithviknishad.ddns.net`
-        % 
-        % TODO: consider changing it to WebSockets based MQTT for improved network performance.
-        % TODO: mask or obscure the url on release.
         client = mqtt("tcp://rithviknishad.ddns.net");
 
-        % To re-order vector data's corresponding topics, or add new topics
-        % modify below vector.
-        % (Note: ORDER OF DATA VECTOR AND TOPIC MUST BE IN SYNC)
         topics = [
             "ps/state/OutputVoltage"
             "ps/state/OutputCurrent"
@@ -32,16 +22,15 @@ function iot_ppc_broadcast_to_mqtt(data)
         ];
     end
 
-    % Convert type Vector<dynamic> to Vector<String>
+    % Stringify all elements of `data`
     data = string(data);
 
-    % Publish all data for available topics.
+    % Publish all elements of `data` to it's respective topics.
     for i = 1 : length(topics)
         publish(client, topics(i), data(i));
     end
-
+    
     timestamp = string(datetime(now, 'ConvertFrom', 'datenum'));
-
     publish(client, "ps/state/LastUpdatedOn", timestamp);
 end
 
